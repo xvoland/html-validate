dce85fb6b7ef0e8a876a4bcf143fe52b3a367c7d
Vitalii Tereshchuk <xvoland@dotoca.net>
2012-09-27 02:52:35 +0300

    mock of PHP

--- assertHTMLValidate.php
+++ assertHTMLValidate.php
@@ -14,26 +14,57 @@ class Assert extends PHPUnit_Framework_Assert
 	protected $_doctype = '<!DOCTYPE html>'
 						 .'<html>';
 
+	protected $_useragent = 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)';
+
 
 	public static function HTMLValidate($html, $output = 'text')
 	{
 		if (!is_string($html))
 		{
 			throw PHPUnit_Util_InvalidArgumentHelper::factory(1, 'string');
-      }
+      	}
 
 		if (!is_string($output) || !in_array($output, $_output))
 		{
 			throw PHPUnit_Util_InvalidArgumentHelper::factory(2, 'string - text/xhtml/html/xml/json/gnu');
-      }
-
+      	}
 
 		$posts = array(
 			'out'     => $output,
 			'content' => $_doctype . '<head><meta charset=utf-8 /><title></title></head><body>'.$html.'</body></html>'
 		);
 
+		$headers = array(
+			'Content-Type' => 'application/xhtml+xml'
+		);
 
-		return null;//self::fail('ky-ky');
+		$curlOpt = array(
+			CURLOPT_USERAGENT      => $this->_useragent,
+			CURLOPT_URL            => $this->_url,
+			CURLOPT_PORT           => $this->_port,
+			CURLOPT_RETURNTRANSFER => true,
+			//CURLOPT_HTTPHEADER => $headers,
+			CURLOPT_POST           => true,
+			CURLOPT_POSTFIELDS     => $posts
+		);
+
+
+		$curl = curl_init();
+		curl_setopt_array($curl, $curlOpt);
+
+
+		if( ! $response = curl_exec($curl))
+		{
+			throw new PHPUnit_Framework_Exception(
+            	sprintf('Can\'t check validation. cURL returning error %s',
+            		trigger_error( curl_error($curl) )
+             	)
+            );
+		}
+
+		//$curl_info = curl_getinfo($curl);
+		curl_close($curl);
+
+		return $response; //self::fail('ky-ky');
 	}
 }
