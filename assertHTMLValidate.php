a18255f10a47412d370ec5f6a70161897797222f
Vitalii Tereshchuk <xvoland@dotoca.net>
2012-09-27 05:14:16 +0300

    response problems are corrected

--- assertHTMLValidate.php
+++ assertHTMLValidate.php
@@ -5,27 +5,46 @@ require_once 'PHPUnit/Framework/Assert.php';
 
 class Assert extends PHPUnit_Framework_Assert
 {
+	/**
+	 * Asserts that a HTML is validate.
+	 *
+	 * @param  string  $html
+	 * @param  string  $output
+	 * @throws PHPUnit_Framework_AssertionFailedError
+	 */
 	public static function HTMLValidate($html, $output = 'text')
 	{
 		$_url       = 'http://html5.validator.nu/';
 		$_port      = null;
 		$_output    = array('xhtml', 'html', 'xml', 'json', 'gnu', 'text');
-		$_doctype   =  '<!DOCTYPE html><html>';
 		$_useragent = 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)';
+
+
+      	if(empty($html))
+      	{
+      		self::assertEmpty($html, self::isEmpty());
+      	}
+
 
 		if (!is_string($html))
 		{
-			throw PHPUnit_Util_InvalidArgumentHelper::factory(1, 'string');
+			throw self::factory(
+				1, 'string'
+			);
       	}
+
 
 		if (!is_string($output) || !in_array($output, $_output))
 		{
-			throw PHPUnit_Util_InvalidArgumentHelper::factory(2, 'string - text/xhtml/html/xml/json/gnu');
+			throw self::factory(
+				2, 'string - text/xhtml/html/xml/json/gnu'
+			);
       	}
+
 
 		$posts = array(
 			'out'     => $output,
-			'content' => $_doctype . '<head><meta charset=utf-8 /><title></title></head><body>'.$html.'</body></html>'
+			'content' => '<!DOCTYPE html><html><head><meta charset=utf-8 /><title></title></head><body>'.$html.'</body></html>'
 		);
 
 		$headers = array(
@@ -59,12 +78,15 @@ class Assert extends PHPUnit_Framework_Assert
 		//$curl_info = curl_getinfo($curl);
 		curl_close($curl);
 
-		if( strpos($response, 'document is valid') === false )
-       	{
-        	self::fail($response);
-        	return $response;
+
+		if(strlen($response) > 100)
+		{
+			if( (strpos($response, 'document is valid') === false) )
+			{
+	        	self::fail($response);
+        	}
        	}
 
-		return true;
+   		return true;
 	}
 }
