1ef5dbefff551470538f661c7c10ca63886f2c5c
Vitalii Tereshchuk <xvoland@dotoca.net>
2012-09-28 04:48:11 +0300

    source code formatting as in https://github.com/pmjones/fig-standards/blob/psr-1-style-guide/proposed/PSR-2-advanced.md

--- assertHTMLValidate.php
+++ assertHTMLValidate.php
@@ -46,22 +46,19 @@ class Assert extends PHPUnit_Framework_Assert
         $_useragent = 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)';
 
 
-        if(empty($html))
-        {
+        if(empty($html)) {
             self::assertEmpty($html, self::isEmpty());
         }
 
 
-        if (!is_string($html))
-        {
+        if (!is_string($html)) {
             throw self::factory(
                 1, 'string'
             );
         }
 
 
-        if (!is_string($output) || !in_array($output, $_output))
-        {
+        if (!is_string($output) || !in_array($output, $_output)) {
             throw self::factory(
                 2, 'string - text/xhtml/html/xml/json'
             );
@@ -73,15 +70,11 @@ class Assert extends PHPUnit_Framework_Assert
             'content' => '<!DOCTYPE html><html><head><meta charset=utf-8 /><title></title></head><body>'.$html.'</body></html>'
         );
 
-        $headers = array(
-            'Content-Type' => 'application/xhtml+xml'
-        );
 
         $curlOpt = array(
             CURLOPT_USERAGENT      => $_useragent,
             CURLOPT_URL            => $_url,
             CURLOPT_PORT           => $_port,
-            //CURLOPT_HTTPHEADER => $headers,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_POST           => true,
             CURLOPT_POSTFIELDS     => $posts
@@ -92,8 +85,7 @@ class Assert extends PHPUnit_Framework_Assert
         curl_setopt_array($curl, $curlOpt);
 
 
-        if( ! $response = curl_exec($curl))
-        {
+        if( ! $response = curl_exec($curl)) {
             throw new PHPUnit_Framework_Exception(
                 sprintf('Can\'t check validation. cURL returning error %s',
                     trigger_error(curl_error($curl))
@@ -104,8 +96,7 @@ class Assert extends PHPUnit_Framework_Assert
         //$curl_info = curl_getinfo($curl);
         curl_close($curl);
 
-        if(stripos($response, 'Error') or stripos($response, 'Warning'))
-        {
+        if(stripos($response, 'Error') or stripos($response, 'Warning')) {
                 self::fail($response);
         }
 
